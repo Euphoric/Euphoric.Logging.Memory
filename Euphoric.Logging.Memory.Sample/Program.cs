73766118a4acb90f0e43041ca4625b8b9b5498e4
Radek Falhar <Radek.Falhar@hotmail.com>
2020-03-03 19:36:15 +0100

    Added sample application.

---
+++ Euphoric.Logging.Memory.Sample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection.Metadata;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Euphoric.Logging.Memory.Sample
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // register memory logger
+            ServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddLogging(logConfig => AddMemoryLogger(logConfig));
+            var sp = serviceCollection.BuildServiceProvider();
+
+            // log messages
+            var logger = sp.GetRequiredService<ILoggerFactory>().CreateLogger("TestProgram");
+
+            logger.LogInformation("Information message {number}", 13);
+
+            logger.LogDebug("Debug message {text}", "PATH");
+
+            logger.LogError(new Exception("Exception text."), "Error message");
+
+            // retrieve logged events from memory logger provider
+            var memoryLogger = sp.GetRequiredService<MemoryLoggerProvider>();
+
+            foreach (var logEntry in memoryLogger.Logs)
+            {
+                Console.WriteLine($"{logEntry.CategoryName}:{logEntry.Level}:{logEntry.Message}");
+            }
+        }
+
+        private static ILoggingBuilder AddMemoryLogger(ILoggingBuilder logConfig)
+        {
+            var provider = new MemoryLoggerProvider();
+            logConfig.Services.AddSingleton(provider);
+            return logConfig.AddProvider(provider);
+        }
+    }
+}
